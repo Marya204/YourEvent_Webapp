d8a78e8046bf84c2e74de2b8b9e8d7bbb0a8414c
MaryamSakouti <112986567+Marya204@users.noreply.github.com>
2024-05-16 22:53:29 +0100

    version 3

--- src/main/java/Controller/LoginServlet.java
+++ src/main/java/Controller/LoginServlet.java
@@ -1,18 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
 package Controller;
 
-import java.io.IOException;
+
 import java.sql.SQLException;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import Model.UserDAO;
-import Model.User;
 
+import model.User;
+import model.UserDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-@WebServlet("/login")
+/**
+ *
+ * @author hp
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -39,4 +49,6 @@ public class LoginServlet extends HttpServlet {
             throw new ServletException("Database access error", e);
         }
     }
+    
+    
 }
