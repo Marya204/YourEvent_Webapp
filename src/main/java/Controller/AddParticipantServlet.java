4207f7fcda73fb8586eebd02ad02161e0fca979d
ASMAEMISBAH22 <155922074+ASMAEMISBAH22@users.noreply.github.com>
2024-05-18 20:41:56 +0100

    version finale

---
+++ src/main/java/Controller/AddParticipantServlet.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Participant;
+import model.ParticipantDAO;
+
+
+
+/**
+ *
+ * @author hp
+ */
+@WebServlet(name = "AddParticipantServlet", urlPatterns = {"/AddParticipantServlet"})
+
+public class AddParticipantServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Retrieve form data
+        String Name = request.getParameter("Name");
+        String Email = request.getParameter("Email");
+        int Eventid = request.getParameter("Eventid");
+
+     
+        Participant participant = new Participant(titre, description, (java.sql.Date) date, lieu, type, status, prix, capacite);
+
+        // Insert the Participant into the database
+        ParticipantDAO participantDAO = new ParticipantDAO();
+        try {
+            participantDAO.addParticipant(participant);
+        } catch (SQLException e) {
+            // Handle database errors when inserting the event
+            e.printStackTrace();
+            // You can redirect to an appropriate error page here
+        }
+
+        // Redirect to the events page after insertion
+        response.sendRedirect("Partcipant.jsp");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Forward POST requests to doGet to handle form submission
+        doGet(request, response);
+    }
+}
---
+++ src/main/java/Controller/ParticipantServlet.java
@@ -0,0 +1,123 @@
+package Controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import model.participantDAO;
+import model.participant;
+
+@WebServlet(name = "ParticipantServlet", urlPatterns = {"/ParticipantServlet"})
+public class ParticipantServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private ParticipantDAO participantDAO;
+
+    @Override
+    public void init() {
+        participantDAO = new ParticipantDAO();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getServletPath();
+        try {
+            switch (action) {
+                case "/new":
+                    showNewForm(request, response);
+                    break;
+                case "/add":
+                    addParticipant(request, response);
+                    break;
+                case "/delete":
+                    deleteParticipant(request, response);
+                    break;
+                case "/edit":
+                    showEditForm(request, response);
+                    break;
+                case "/update":
+                    updateParticipant(request, response);
+                    break;
+                default:
+                    listParticipant(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    private void listParticipant(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        List<Participant> listparticipant = participantDAO.getAllParticipants();
+        request.setAttribute("listparticipant", listparticipant);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("participant.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("AddParticipant.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, ServletException, IOException {
+        int participantID = Integer.parseInt(request.getParameter("ParticipantID"));
+        Participant existingParticipant = participantDAO.getParticipant(participantID);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("AddParticipant.jsp");
+        request.setAttribute("participant", existingParticipant);
+        dispatcher.forward(request, response);
+    }
+
+    private void addParticipant(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        String name = request.getParameter("Name");
+        String email = request.getParameter("Email");
+        
+        int eventId = Integer.parseInt(request.getParameter("Eventid"));
+
+        Participant participant = new Participant(name,email,eventId);
+        try {
+            participantDAO.addparticipant(participant);
+            response.sendRedirect("ParticipantServlet");
+        } catch (SQLException e) {
+            throw new ServletException(e);
+        }
+    }
+    
+
+    private void updateParticipant(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        int Participantid = Integer.parseInt(request.getParameter("ParticipantID"));
+        String name = request.getParameter("Name");
+        String email = request.getParameter("Email");
+        int eventId = Integer.parseInt(request.getParameter("Eventid"));
+       
+        Participant participant = new Participant(ParticipantId,name,email,eventId);
+        participantDAO.updateParticipant(participant);
+        response.sendRedirect("ParticipantServlet");
+    }
+
+    private void deleteParticipant(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        int Participantid = Integer.parseInt(request.getParameter("ParticipantID"));
+        participantDAO.deleteParticipant(Participantid);
+        response.sendRedirect("ParticipantServlet");
+    }
+}
