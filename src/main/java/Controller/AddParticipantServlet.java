de44c3a9e9ece3950b6a8d20bae99a43eea10a3e
MaryamSakouti <112986567+Marya204@users.noreply.github.com>
2024-05-19 12:15:24 +0100

    version finale

--- src/main/java/Controller/AddParticipantServlet.java
+++ src/main/java/Controller/AddParticipantServlet.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package Controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,10 +27,10 @@ public class AddParticipantServlet extends HttpServlet {
         // Retrieve form data
         String Name = request.getParameter("Name");
         String Email = request.getParameter("Email");
-        int Eventid = request.getParameter("Eventid");
+        int Eventid= Integer.parseInt(request.getParameter("Eventid"));
 
      
-        Participant participant = new Participant(Name,Email,Eventid);
+        Participant participant = new  Participant (Name, Email, Eventid);
 
         // Insert the Participant into the database
         ParticipantDAO participantDAO = new ParticipantDAO();
@@ -47,9 +41,8 @@ public class AddParticipantServlet extends HttpServlet {
             e.printStackTrace();
             // You can redirect to an appropriate error page here
         }
-
         // Redirect to the events page after insertion
-        response.sendRedirect("Partcipant.jsp");
+        response.sendRedirect("Participant.jsp");
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -58,3 +51,5 @@ public class AddParticipantServlet extends HttpServlet {
         doGet(request, response);
     }
 }
+ 
+
